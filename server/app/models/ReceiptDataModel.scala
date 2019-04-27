06c884f83fbb0b032e1b57ffea99bf60eb1f7e80
Nihil Patel <Nihil@nihil-patels-macbook-pro.local>
2019-04-27 15:22:24 -0500

    Start on Receipt Data model changes

--- server/app/models/ReceiptDataModel.scala
+++ server/app/models/ReceiptDataModel.scala
@@ -1,5 +1,10 @@
 package models
-
+import slick.jdbc.MySQLProfile.api._
+import scala.concurrent.duration.Duration
+import scala.concurrent.Await
+import scala.concurrent.Future
+import Tables._
+import scala.concurrent.ExecutionContext
 case class Receipt(userID: Int, receiptID: Int, vendorID: Int, item: String, description: String)
 
 object ReceiptDataModel {
