f52ca1b16876205aa291c0b76490227a5af899f0
Nihil Patel <Nihil@nihil-patels-macbook-pro.local>
2019-04-27 15:22:05 -0500

    Start making changes to models to incorporate slick

--- server/app/models/UserModel.scala
+++ server/app/models/UserModel.scala
@@ -1,62 +1,67 @@
 package models
 
 import scala.collection.mutable
-
+import slick.jdbc.MySQLProfile.api._
+import scala.concurrent.duration.Duration
+import scala.concurrent.Await
+import scala.concurrent.Future
+import Tables._
+import scala.concurrent.ExecutionContext
 case class User(id: Int, name: String, username: String, pw: String, email: String,phone: Int)
 
 object UserModel {
   var users = mutable.Map[String, String]()                    // Username -> password
   var tasks = mutable.Map[String, mutable.Seq[Seq[String]]]()  // Username -> tasks
   var admin = mutable.Buffer[String]()                         // Usernames of the admin
-  
+
   // when a new user has been created.
   def addUser(name:String, pw:String) = {
     if(!checkName(name)) {
       users(name) = pw
-      tasks(name) = mutable.Seq[Seq[String]]() // users tasks are initially empty. 
-    } 
+      tasks(name) = mutable.Seq[Seq[String]]() // users tasks are initially empty.
+    }
   }
-  
+
   // checks if name is already used
-  def checkName(name:String): Boolean = {
+  def checkName(name:String): Future[Option[Int]] = {
     return users.contains(name)
   }
-  
+
   // checks if password is correct
   def checkPassword(name: String, pw: String): Boolean = {
     return users.contains(name) && users(name) == pw
   }
 
-  // returns the tasks of a user. 
+  // returns the tasks of a user.
   def getTasks(name:String): Seq[Seq[String]] = {
     if(checkName(name)) {
       tasks(name)
-      
-      
-      
+
+
+
     }
     else {
       Seq(Seq())
     }
   }
-  
+
   def addTask(name: String, comps: String*): Boolean = {
     println(comps)
-    
+
     if(checkName(name)) {
       tasks(name) :+= comps // append task to the user's tasks
       return true
     }
     return false
   }
-  
+
   def removeTask(name:String, taskid: Int) = {
     tasks(name) = tasks(name).zipWithIndex.filter(_._2 != taskid-1).map(_._1)
   }
-  
+
   def clearModel() {
     users.clear()     // Username -> password
     tasks.clear()  // Username -> tasks
   }
-  
+
 }
