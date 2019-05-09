5fe81c802e6bbdac2051758686170af4b20543b8
Shea Luikart <cluikart@trinity.edu>
2019-05-09 16:22:55 -0500

    Modify Tool

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -5,10 +5,70 @@
 //$("h1").css("color", "red")
 //document.getElementById("head").style.color = "red";
 
+let mod = false;
+
+function expand(imgs) {
+	if(mod === false){
+	  var expandImg = document.getElementById("expandedImg");
+	  var imgText = document.getElementById("imgtext");
+	  expandImg.src = imgs.src;
+	  imgText.innerHTML = imgs.alt;
+	  expandImg.parentElement.style.display = "block";
+	}
+}
+
+function on() {
+	if(mod === false){
+		document.getElementById("overLay").style.display = "block";
+	}
+}
+
+function off() {
+  document.getElementById("overLay").style.display = "none";
+}
+
+document.getElementById("myBtn").style.display = "none"
+
+
 $("#delete").click(function() {
 	alert( "Handler for .click() called." );
 });
-//$("#delete").css("color", "red");
+
+var modal = document.getElementById('myModal');
+
+
+
+//Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+
+//When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = "none";
+	mod = false;
+	$("#modify").css("color", "#fff");
+
+}
+
+//When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		mod = false;
+		$("#modify").css("color", "#fff");
+		modal.style.display = "none";
+	}
+}
+
+$("#modify").click(function() {
+	$(this).css("color", "red");
+	mod = true;
+})
+function modifyWindow() {
+	if(mod === true){
+		modal.style.display = "block";
+	}
+}
 
 function filterByDesc(desc) {
 	let imgsrc = "/images/"+desc;
@@ -37,14 +97,14 @@ function loadGallery() {
 		$.each(data, function(i, item) {
 			if(i%3 === 1){
 				console.log("Div 1 "+item.imglink)
-				$("<img>").attr({"src": item.imglink, "class": "column", "style": "width: 100%", "onclick": "expand(this); on()"}).appendTo("#c1");
+				$("<img>").attr({"src": item.imglink, "class": "column", "style": "width: 100%", "onclick": "expand(this); on(); modifyWindow()"}).appendTo("#c1");
 			} else if(i%3 === 2){
 				console.log("Div 2 "+item.imglink)
-				$("<img>").attr({"src": item.imglink, "class": "column", "style": "width: 100%", "onclick": "expand(this); on()"}).appendTo("#c2");
+				$("<img>").attr({"src": item.imglink, "class": "column", "style": "width: 100%", "onclick": "expand(this); on(); modifyWindow()"}).appendTo("#c2");
 				//$("<img>").attr("src", item.imglink).appendTo("#c2");
 			} else if(i%3 === 0){
 				console.log("Div 3 "+item.imglink)
-				$("<img>").attr({"src": item.imglink, "class": "column", "style": "width: 100%", "onclick": "expand(this); on()"}).appendTo("#c3");
+				$("<img>").attr({"src": item.imglink, "class": "column", "style": "width: 100%", "onclick": "expand(this); on(); modifyWindow();"}).appendTo("#c3");
 				//$("<img>").attr("src", item.imglink).appendTo("#c3");
 			}
 		});
